a04195b9566e19b892a607dd702835154868006a
Swapnil Nangare <7smn2219@gmail.com>
2020-11-10 18:45:18 +0530

    postcss-preset-env add in the place of autoprifixer

--- webpack.config.js
+++ webpack.config.js
@@ -2,62 +2,69 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-        devServer: {
-            contentBase: path.resolve(__dirname, 'demo'),
-            compress: true,
-            publicPath: "demo",
-            writeToDisk: true
-        },
-        entry: './src/js/app.js',
-        output: {
-            filename: 'app.js',
-            path: path.resolve(__dirname, 'demo/js'),
-            publicPath: 'demo'
-        },
-        module: {
-            rules: [{
-                    test: /\.(scss)$/,
-                    use: [{
-                            loader: MiniCssExtractPlugin.loader,
-                        },
-                        {
-                            loader: 'css-loader'
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                plugins: function() {
-                                    return [
-                                        require('autoprefixer')
-                                    ];
-                                }
+    devServer: {
+        contentBase: path.resolve(__dirname, 'demo'),
+        compress: true,
+        publicPath: "demo",
+        writeToDisk: true
+    },
+    entry: './src/js/app.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'demo/js'),
+        publicPath: 'demo'
+    },
+
+    module: {
+        rules: [{
+                test: /\.(scss)$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        'postcss-preset-env',
+                                        {
+                                            //options
+                                        }
+                                    ]
+                                ]
                             }
-                        },
-                        {
-                            loader: 'sass-loader'
                         }
-                    ]
-                },
-                {
-                    test: /\.(eot|woff|woff2|ttf|svg)(\?\S*)?$/,
-                    use: [{
-                            loader: 'file-loader',
-                            options: {
-                                name: '[name].[ext]',
-                                outputPath: '../fonts/',
-                                publicPath: '../fonts/'
-                            }
-                        ]
+                    },
+                    {
+                        loader: 'sass-loader'
                     }
                 ]
+
             },
-            plugins: [
-                new MiniCssExtractPlugin({
-                    filename: '../css/app.css'
-                }),
-                new webpack.ProvidePlugin({
-                    $: 'jquery',
-                    jQuery: 'jquery'
-                })
-            ]
-        }
+            {
+                test: /\.(eot|woff|woff2|ttf|svg)(\?\S*)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: '../fonts/',
+                        publicPath: '../fonts/'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/app.css'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ]
+}
